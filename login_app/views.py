3048086622782bf3348cff0fcec93fb891e2acfb
Adnan Jamil <jamil.adnan@live.com>
2025-06-27 23:45:34 +0200

    login page finalized

--- login_app/views.py
+++ login_app/views.py
@@ -8,15 +8,20 @@ def login(request):
 
 def userAuthenticaton(request):
     if request.method == "POST":
+        
         userUsername = request.POST.get("username")
         userPassword = request.POST.get("pass")
+        
         try:
-            data = User.objects.all(username = userUsername, password = userPassword)
-            request.session ['userId'] = data.id
+            user = User.objects.get(username = userUsername, password = userPassword)
+            request.session ['userId'] = user.id
             return redirect ('blog')
         
         except User.DoesNotExist:
             error = "Invalid username or password"
             messages.error (request, error)
             return redirect ('login')
+    else:
+        return redirect ('login')
+        
     
